11f78f5049e7b48160cc8df8703f3d0d841e6f93
shadat-hossan <shadathossan3500@gmail.com>
2024-02-13 18:14:59 +0600

    layout and Flash Sales section create

--- cliend/assets/js/index.js
+++ cliend/assets/js/index.js
@@ -30,31 +30,46 @@ document.addEventListener("scroll", function () {
   }
 });
 
-//
+// Flash Sales Time functionality
 
-const storyReadMor = document.querySelector(".storyReadMor");
-const hiddenText = document.querySelector("#hiddenText");
-const dot = document.querySelector("#dot");
-if (storyReadMor) {
-  storyReadMor.addEventListener("click", () => {
-    if (dot.style.display != "none") {
-      hiddenText.style.display = "inline";
-      dot.style.display = "none";
-      storyReadMorImage.style.transform = "rotate(90deg)";
-      storyReadMor.innerHTML = `Read Less <img
-            src="assets/img/logo/arrayUp.svg"
-            alt="Read More Icon"
-            id="storyReadMorImage"
-          />`;
-    } else {
-      hiddenText.style.display = "none";
-      dot.style.display = "inline";
-      storyReadMorImage.style.transform = "rotate(0deg)";
-      storyReadMor.innerHTML = `Read More <img
-            src="assets/img/logo/arrayDown.svg"
-            alt="Read More Icon"
-            id="storyReadMorImage"
-          />`;
-    }
-  });
+function getTimeRemaining(endtime) {
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor((t / 1000) % 60);
+  var minutes = Math.floor((t / 1000 / 60) % 60);
+  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+  var days = Math.floor(t / (1000 * 60 * 60 * 24));
+  return {
+    total: t,
+    days: days,
+    hours: hours,
+    minutes: minutes,
+    seconds: seconds,
+  };
 }
+
+function initializeClock(id, endtime) {
+  var clock = document.getElementById(id);
+  var daysSpan = clock.querySelector(".days");
+  var hoursSpan = clock.querySelector(".hours");
+  var minutesSpan = clock.querySelector(".minutes");
+  var secondsSpan = clock.querySelector(".seconds");
+
+  function updateClock() {
+    var t = getTimeRemaining(endtime);
+
+    daysSpan.innerHTML = ("0" + t.days).split(-2);
+    hoursSpan.innerHTML = ("0" + t.hours).slice(-2);
+    minutesSpan.innerHTML = ("0" + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ("0" + t.seconds).slice(-2);
+
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
+    }
+  }
+
+  updateClock();
+  var timeinterval = setInterval(updateClock, 1000);
+}
+
+var deadline = new Date(Date.parse(new Date()) + 3 * 24 * 60 * 60 * 1000); // Day : hours : minutes : secounds
+initializeClock("clockdiv", deadline);
