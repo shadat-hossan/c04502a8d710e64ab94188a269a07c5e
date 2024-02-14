7fcca9fd847d2788d8c1acac0980fb49edbb4bcc
shadat-hossan <shadathossan3500@gmail.com>
2024-02-14 13:33:37 +0600

    merge header Section

--- cliend/assets/js/index.js
+++ cliend/assets/js/index.js
@@ -1,75 +1,93 @@
-//menu responsive click
-document.addEventListener("DOMContentLoaded", function () {
-  var menuToggle = document.querySelector(".menu-toggle");
-  var nav = document.querySelector(".navbar-menu");
-
-  menuToggle.addEventListener("click", function () {
-    nav.classList.toggle("active");
-  });
-});
-
-// menu scroll effect
-document.addEventListener("scroll", function () {
-  var navbarContainer = document.querySelector(".navbar-container");
-  var logo = document.querySelector(".slogo");
-  var menuItems = document.querySelectorAll(
-    ".navbar-container .navbar-menu ul li a"
-  );
-  if (document.documentElement.scrollTop > 80) {
-    navbarContainer.classList.add("shrink");
-    logo.classList.add("logoShirnk");
-    menuItems.forEach(function (menuItem) {
-      menuItem.classList.add("menuItemsAllA");
-    });
-  } else {
-    navbarContainer.classList.remove("shrink");
-    logo.classList.remove("logoShirnk");
-    menuItems.forEach(function (menuItem) {
-      menuItem.classList.remove("menuItemsAllA");
-    });
-  }
+jQuery(".menu_bar").click(function () {
+  jQuery(".categorie_dropdown").slideToggle(400);
 });
 
 // Flash Sales Time functionality
 
-function getTimeRemaining(endtime) {
-  var t = Date.parse(endtime) - Date.parse(new Date());
-  var seconds = Math.floor((t / 1000) % 60);
-  var minutes = Math.floor((t / 1000 / 60) % 60);
-  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-  var days = Math.floor(t / (1000 * 60 * 60 * 24));
-  return {
-    total: t,
-    days: days,
-    hours: hours,
-    minutes: minutes,
-    seconds: seconds,
-  };
-}
+// function getTimeRemaining(endtime) {
+//   var t = Date.parse(endtime) - Date.parse(new Date());
+//   var seconds = Math.floor((t / 1000) % 60);
+//   var minutes = Math.floor((t / 1000 / 60) % 60);
+//   var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+//   var days = Math.floor(t / (1000 * 60 * 60 * 24));
+//   return {
+//     total: t,
+//     days: days,
+//     hours: hours,
+//     minutes: minutes,
+//     seconds: seconds,
+//   };
+// }
 
-function initializeClock(id, endtime) {
-  var clock = document.getElementById(id);
-  var daysSpan = clock.querySelector(".days");
-  var hoursSpan = clock.querySelector(".hours");
-  var minutesSpan = clock.querySelector(".minutes");
-  var secondsSpan = clock.querySelector(".seconds");
+// function initializeClock(id, endtime) {
+//   var clock = document.getElementById(id);
+//   var daysSpan = clock.querySelector(".days");
+//   var hoursSpan = clock.querySelector(".hours");
+//   var minutesSpan = clock.querySelector(".minutes");
+//   var secondsSpan = clock.querySelector(".seconds");
 
-  function updateClock() {
-    var t = getTimeRemaining(endtime);
+//   function updateClock() {
+//     var t = getTimeRemaining(endtime);
 
-    daysSpan.innerHTML = ("0" + t.days).split(-2);
-    hoursSpan.innerHTML = ("0" + t.hours).slice(-2);
-    minutesSpan.innerHTML = ("0" + t.minutes).slice(-2);
-    secondsSpan.innerHTML = ("0" + t.seconds).slice(-2);
+//     daysSpan.innerHTML = ("0" + t.days).split(-2);
+//     hoursSpan.innerHTML = ("0" + t.hours).slice(-2);
+//     minutesSpan.innerHTML = ("0" + t.minutes).slice(-2);
+//     secondsSpan.innerHTML = ("0" + t.seconds).slice(-2);
 
-    if (t.total <= 0) {
-      clearInterval(timeinterval);
+//     if (t.total <= 0) {
+//       clearInterval(timeinterval);
+//     }
+//   }
+
+//   updateClock();
+//   var timeinterval = setInterval(updateClock, 1000);
+// }
+
+// var deadline = new Date(Date.parse(new Date()) + 3 * 24 * 60 * 60 * 1000); // Day : hours : minutes : secounds
+// initializeClock("clockdiv", deadline);
+
+jQuery(document).ready(function () {
+  jQuery(document).on("click", function (e) {
+    if (!$(e.target).closest(".menu_box").length) {
+      $(".categorie_dropdown").slideUp();
     }
-  }
+  });
+  jQuery(".menu_bar").on("click", function () {
+    jQuery(this).siblings(".categorie_dropdown").slideToggle();
+  });
+});
 
-  updateClock();
-  var timeinterval = setInterval(updateClock, 1000);
+$(document).ready(function () {
+  $(".user_login").click(function () {
+    var docClick = function (ev) {
+      if (!$(ev.target).hasClass("user_login")) {
+        closeDropdown();
+      }
+    };
+    var closeDropdown = function () {
+      $(".user_dropdown").removeClass("show-menu");
+      $(document).unbind("click", docClick);
+    };
+
+    if ($(".user_dropdown").hasClass("show-menu")) {
+      closeDropdown();
+    } else {
+      $(document).bind("click", docClick);
+      $(".user_dropdown").addClass("show-menu");
+    }
+
+    $(".user_dropdown > li").click(function () {
+      closeDropdown();
+    });
+    $(".user_dropdown > li").click(function () {
+      $(".user_login").html($(this).html());
+    });
+  });
+});
+
+if (matchMedia("only screen and (max-width: 768px)").matches) {
+  $(".categorie_dropdown ul > li.drop_child > a").click(function () {
+    $(this).toggleClass("active");
+    $(this).next("ul").slideToggle("slow");
+  });
 }
-
-var deadline = new Date(Date.parse(new Date()) + 3 * 24 * 60 * 60 * 1000); // Day : hours : minutes : secounds
-initializeClock("clockdiv", deadline);
