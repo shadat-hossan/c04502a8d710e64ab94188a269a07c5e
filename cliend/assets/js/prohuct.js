618883a666ec43fb375c8c5b7dcb7e1c3f91154d
shadat-hossan <shadathossan3500@gmail.com>
2024-02-18 18:23:50 +0600

    added singal Product page

--- cliend/assets/js/prohuct.js
+++ cliend/assets/js/prohuct.js
@@ -1,19 +1,45 @@
-// The Progrem is Count Dowen on product
+// singal Product Togal Image
 
-const Increment = document.querySelector(".increment");
-const Dickrement = document.querySelector(".dickrement");
-let numberCount = document.querySelector("#numberCount");
+const allGogalImage = document.querySelectorAll(".filtaringImageItem");
+let productFinalImageShow = document.querySelector(
+  ".productFinalImageShow img"
+);
+let singalProductPrice = document.querySelector(".singalProductPrice input");
+let productContite = document.querySelector(".productContite");
+const increment = document.querySelector(".increment");
+const dickrement = document.querySelector(".dickrement");
+let productContiteCount =  1;
 
-let count = 1;
+const singalProductPriceInVa = document.querySelector(".singalProductPrice input")
 
-Increment.addEventListener("click", function () {
-  count += 1;
-  numberCount.innerHTML = count;
-});
 
-Dickrement.addEventListener("click", function () {
-  if (count > 1) {
-    count -= 1;
-    numberCount.innerHTML = count;
+increment.addEventListener("click", function(){
+  productContiteCount += 1;
+  productContite.value = productContiteCount
+}) 
+
+
+dickrement.addEventListener("click", function(){
+  if(productContiteCount > 1){
+    productContiteCount -= 1;
+    productContite.value = productContiteCount
   }
+}) 
+
+
+
+let myimage = document.querySelector("#myimage");
+let myresult = document.querySelector("myresult");
+
+console.log(productFinalImageShow.src);
+
+allGogalImage.forEach(function (i) {
+  i.addEventListener("click", function () {
+    productFinalImageShow.src = i.childNodes[1].src;
+    console.log(i.childNodes[1].src);
+  });
 });
+
+
+
+
