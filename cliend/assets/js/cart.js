39042784ed6a2b071060f30770956c62e31b3a67
jahangir6085 <jahangiruzzal@gmail.com>
2024-02-19 15:53:56 +0600

    add order page

--- cliend/assets/js/cart.js
+++ cliend/assets/js/cart.js
@@ -101,3 +101,28 @@ $(document).ready(function() {
     })();
 
   });
+
+
+
+function closeForm() {
+  $('.form-popup-bg').removeClass('is-visible');
+}
+$(document).ready(function($) {
+  
+  /* Contact Form Interactions */
+  $('.btnOpenForm').on('click', function(event) {
+    event.preventDefault();
+
+    $('.form-popup-bg').addClass('is-visible');
+    $('body').addClass('body-fixed');
+  });
+    //close popup when clicking x or off popup
+  $('.form-popup-bg').on('click', function(event) {
+    if ($(event.target).is('.form-popup-bg') || $(event.target).is('.btnCloseForm')) {
+      event.preventDefault();
+      $(this).removeClass('is-visible');
+      $('body').removeClass('body-fixed');
+    }
+  });
+  
+  });
