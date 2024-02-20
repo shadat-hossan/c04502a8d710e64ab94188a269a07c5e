8eb3d7ea016aed95602b08f07549b91fae99f990
jahangir6085 <jahangiruzzal@gmail.com>
2024-02-20 12:53:03 +0600

    add sign up page and login page

--- cliend/assets/js/cart.js
+++ cliend/assets/js/cart.js
@@ -125,4 +125,16 @@ $(document).ready(function($) {
     }
   });
   
+  
+  $(".toggle-password").click(function() {
+      $(this).toggleClass("fa-eye fa-eye-slash");
+      input = $(this).parent().find("input");
+      if (input.attr("type") == "password") {
+          input.attr("type", "text");
+      } else {
+          input.attr("type", "password");
+      }
+  });
+
+
   });
