f769932132279b5ad36153f231878c0f31212549
shadat-hossan <shadathossan3500@gmail.com>
2024-02-24 15:43:16 +0600

    added style user review page

--- cliend/assets/js/userAcout.js
+++ cliend/assets/js/userAcout.js
@@ -28,3 +28,52 @@ $('#userImage').change(function(){
   };
   reader.readAsDataURL(this.files[0]);
 });
+
+
+// Review Star Rating Script
+
+let star = document.querySelectorAll(".starRatingInput");
+let showValue = document.querySelector("#rating-value-pient");
+const reviewMessage = document.querySelector(".userReciewProductRatingStarMesage");
+
+let showValueTextContant =  showValue.textContent
+
+for (let i = 0; i < star.length; i++) {
+  star[i].addEventListener("click", function () {
+    i = this.value;
+    showValue.innerHTML = i;
+    if(i <= 1){
+      reviewMessage.style.padding = "8px 10px"
+      reviewMessage.style.background = "#fd9e9e"
+      reviewMessage.style.borderColor = "#a10000"
+      reviewMessage.innerHTML = "Terrible 😔"
+    }
+    else if( i <= 2){
+    reviewMessage.style.padding = "8px 10px"
+    reviewMessage.style.background = "#a96b6b"
+    reviewMessage.style.borderColor = "#965353"
+    reviewMessage.innerHTML = "Poor 😔"
+    }
+    else if(i <= 3){
+      reviewMessage.style.padding = "8px 10px"
+      reviewMessage.style.background = "#d1bd8d"
+    reviewMessage.style.borderColor = "#beb192"
+    reviewMessage.innerHTML = "Fair 🙁"
+    }
+    else if(i <= 4){
+      reviewMessage.style.padding = "8px 10px"
+      reviewMessage.style.background = "#10751a"
+      reviewMessage.style.borderColor = "#07570f"
+    reviewMessage.innerHTML = "Good 😲"
+    }
+    else if(i <= 5){
+      reviewMessage.style.padding = "8px 10px"
+    reviewMessage.style.background = "#008000"
+    reviewMessage.style.borderColor = "#005a00"
+    reviewMessage.innerHTML = "Excellent 🫡"
+    }
+  });
+}
+
+
+
